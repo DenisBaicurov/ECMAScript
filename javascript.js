d1fd88a1ee6286b1375097a8dc087933c5676b1c
DenisBaicurov <denisbaicurov@mail.ru>
2023-10-09 20:51:38 +0400

    выполнили 3 задание

---
+++ javascript.js
@@ -0,0 +1,141 @@
+/**
+ * Урок 3. Объектно-ориентированное программирование в Javascript
+Домашнее задание
+Задание 1: ""Управление персоналом компании""
+
+Реализуйте класс Employee (сотрудник), который имеет следующие свойства и методы:
+
+Свойство name (имя) - строка, имя сотрудника.
+Метод displayInfo() - выводит информацию о сотруднике (имя).
+Реализуйте класс Manager (менеджер), который наследует класс Employee и имеет дополнительное свойство и метод:
+
+Свойство department (отдел) - строка, отдел, в котором работает менеджер.
+Метод displayInfo() - переопределяет метод displayInfo() родительского класса и выводит информацию о менеджере (имя и отдел).
+// Пример использования классов
+const employee = new Employee(""John Smith"");
+employee.displayInfo();
+// Вывод:
+// Name: John Smith
+
+const manager = new Manager(""Jane Doe"", ""Sales"");
+manager.displayInfo();
+// Вывод:
+// Name: Jane Doe
+// Department: Sales
+
+""Управление списком заказов""
+
+Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
+
+Свойство orderNumber (номер заказа) - число, уникальный номер заказа.
+Свойство products (продукты) - массив, содержащий список продуктов в заказе.
+Метод addProduct(product) - принимает объект product и добавляет его в список продуктов заказа.
+Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+
+// Пример использования класса
+class Product {
+constructor(name, price) {
+this.name = name;
+this.price = price;
+}
+}
+
+const order = new Order(12345);
+
+const product1 = new Product(""Phone"", 500);
+order.addProduct(product1);
+
+const product2 = new Product(""Headphones"", 100);
+order.addProduct(product2);
+
+console.log(order.getTotalPrice()); // Вывод: 600
+ * 
+ * 
+ * 
+ */
+
+class Employee{
+name;
+constructor(name){
+    this.name=name;
+}
+
+displayInfo(){
+console.log("Name: "+this.name) ;   
+}
+
+}
+const employee=new Employee("Денис Байцуров");
+employee.displayInfo();
+
+
+
+class Manager extends Employee {
+
+    department;
+    constructor(name,department){
+      super(name);
+        this.department=department;
+    }
+
+    displayInfo(){console.log("Name: "+this.name+"\n"+"Department: "+this.department)}
+
+}
+const manager=new Manager("Денис Байцуров","1 отдел");
+manager.displayInfo();
+
+
+
+class Product{
+    name;
+    price;
+    constructor(name,price){
+    this.name=name;
+    this.price=price;
+    
+    }
+    
+    
+    
+    }
+
+
+
+class Order{
+products=[];
+sum=0;
+    constructor(orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+   
+    
+    addProduct( product){
+
+this.products.push(product.name);
+  this.sum+=product.price;
+      
+    }
+
+getTotalName(){
+
+    return this.products
+}
+
+    getTotalPrice(){
+return this.sum;
+    }
+
+}
+
+
+
+const order = new Order(12345);
+
+const product1 = new Product("Phone", 500);
+order.addProduct(product1);
+
+const product2 = new Product("Headphones", 100);
+order.addProduct(product2);
+console.log("Список товаров: "+  order.getTotalName());
+console.log("Общая сумма товаров: "+ order.getTotalPrice());
+
